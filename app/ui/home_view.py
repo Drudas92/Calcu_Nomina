9a2c002aa5384f33c768bef11bca5ab8f94f9b29
Diego Rudas Castaño <diego.rudas@ucp.edu.co>
2026-07-27 23:23:05 -0500

    feat: agregar barra lateral inicial

--- app/ui/home_view.py
+++ app/ui/home_view.py
@@ -2,18 +2,58 @@ import customtkinter as ctk
 
 
 def crear_home(app):
+
+    # Frame principal
+    frame_principal = ctk.CTkFrame(app)
+    frame_principal.pack(fill="both", expand=True)
+
+    # ==========================
+    # Barra lateral
+    # ==========================
+    sidebar = ctk.CTkFrame(
+    frame_principal,
+    width=220,
+    corner_radius=0,
+    fg_color="#1f6aa5"
+)
+
+    sidebar.pack(side="left", fill="y")
+    sidebar.pack_propagate(False)
+    menu = ctk.CTkLabel(
+    sidebar,
+    text="MENÚ",
+    font=("Arial", 22, "bold"),
+    text_color="white"
+)
+
+    menu.pack(pady=30)
+
+    # ==========================
+    # Área principal
+    # ==========================
+    contenido = ctk.CTkFrame(
+    frame_principal,
+    fg_color="transparent"
+)
+
+    contenido.pack(side="right", fill="both", expand=True)
+
+    # ==========================
     # Título
+    # ==========================
+
     titulo = ctk.CTkLabel(
-        app,
+        contenido,
         text="Calcu_Nomina",
         font=("Arial", 28, "bold")
     )
+
     titulo.pack(pady=30)
 
-    # Subtítulo
     subtitulo = ctk.CTkLabel(
-        app,
-        text="Sistema de Gestión y Validación de Nómina",
+        contenido,
+        text="Sistema Inteligente de Gestión y Validación de Nómina",
         font=("Arial", 16)
     )
+
     subtitulo.pack()
